ae3f7ab088df1b88f8fc0ec8941e47e0cb6b2a77
Deirdre Cruice <dmcruice@gmail.com>
2020-11-02 18:24:28 -0500

    questions updated

--- index.js
+++ index.js
@@ -1,54 +1,78 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
+const util = require('util');
 
-// variables for questions
-
-
-// variables for answers
-
+// const generateREADME = require("./utils/generateMarkdown");
+// const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
 const questions = [
-    "title", 
-    "description", 
-    "toc", 
-    "license"
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-    
-    const filename = data.name.toLowerCase().split(' ').join('') + ".json";
-    
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
-
-        if (err) {
-          return console.log(err);
-        }
-    
-        console.log("Success!");
-    });
-}
-
-// function to initialize program
-function init() {
-    
-   inquirer.prompt([
+    {
+        type: "input",
+        name: "createdBy",
+        message: "What is your name?",
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+    },
     {
         type: "input",
         name: "title",
-        message: "What is your title?"
-    }, 
+        message: "What is the name of your application?",
+    },
     {
         type: "input",
-    }
-   ]).then(function(data) {
+        name: "description",
+        message: "Enter a description of your application: ",
+    },
+    {
+        type: "input",
+        name: "linktodeployed",
+        message: "Link to the deployed Application: "
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Select a license:",
+        choices: [
+            "MIT",
+            "Apache",
+        ],  
+    },
+];
 
-    writeToFile();
-   })
+// // function to write README file
+// function writeToFile(fileName, data) {
+    
+//     const filename = data.name.toLowerCase().split(' ').join('') + ".json";
+    
+//     fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+
+//         if (err) {
+//           return console.log(err);
+//         }
+    
+//         console.log("Success!");
+//     });
+// }
+
+// // function to initialize program
+// function init() {
+    
+//    inquirer.prompt(questions).then(function(data) {
+
+//     writeToFile();
+//    })
 
     
-}
+// }
 
 // function call to initialize program
 init();
